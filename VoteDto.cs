24d474146d03b555cece3029ba3c4999b3e742df
akarsh363 <122525163+akarsh363@users.noreply.github.com>
2025-09-22 20:25:36 +0530

    Delete VoteDto.cs

--- VoteDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FNF_PROJ.DTOs
-{
-    public class VoteDto
-    {
-        public int? PostId { get; set; }
-        public int? CommentId { get; set; }
-        public string VoteType { get; set; } = null!; // "Upvote" or "Downvote"
-    }
-}
