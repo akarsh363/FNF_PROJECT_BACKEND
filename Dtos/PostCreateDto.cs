c65ce470b27b9a90de5860c1588d1bd3c6f6d777
akarsh363 <122525163+akarsh363@users.noreply.github.com>
2025-09-23 20:10:45 +0530

    Add files via upload

--- Dtos/PostCreateDto.cs
+++ Dtos/PostCreateDto.cs
@@ -1,11 +1,9 @@
-﻿namespace FNF_PROJ.DTOs
+﻿public class PostCreateDto
 {
-    public class PostCreateDto
-    {
-        public string Title { get; set; } = null!;
-        public string Body { get; set; } = null!;
-        public int DeptId { get; set; }
-        public List<int>? TagIds { get; set; } // Tags assigned
-        public List<IFormFile>? Attachments { get; set; }
-    }
+    public string Title { get; set; } = "";
+    // This will hold the raw JSON string provided by client (BodyJson)
+    public string Body { get; set; } = "";
+    public int DeptId { get; set; }
+    public List<int>? TagIds { get; set; }
+    public List<IFormFile>? Attachments { get; set; }
 }
